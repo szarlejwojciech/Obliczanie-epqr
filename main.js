e406417ff3679d89aa5bae55cc0277e7a8a41196
Wojtek Szarlej <szarlej.wojciech@gmail.com>
2019-09-30 12:17:42 +0200

    add rwd style

--- main.js
+++ main.js
@@ -149,8 +149,8 @@ const defaultAnswers = () => {
 
 const stens = {
   students: {
-    male: {
-      psyhotyzm: {
+    psyhotyzm: {
+      male: {
         '0': 1,
         '1': 1,
         '2': 1,
@@ -183,10 +183,240 @@ const stens = {
         '29': 10,
         '30': 10,
         '31': 10,
-        '32': 10,
+        '32': 10
+      },
+      female: {
+        "0": 1,
+        "1": 1,
+        "2": 1,
+        "3": 2,
+        "4": 3,
+        "5": 4,
+        "6": 4,
+        "7": 5,
+        "8": 6,
+        "9": 6,
+        "10": 7,
+        "11": 7,
+        "12": 8,
+        "13": 8,
+        "14": 9,
+        "15": 9,
+        "16": 9,
+        "17": 10,
+        "18": 10,
+        "19": 10,
+        "20": 10,
+        "21": 10,
+        "22": 10,
+        "23": 10,
+        "24": 10,
+        "25": 10,
+        "26": 10,
+        "27": 10,
+        "28": 10,
+        "29": 10,
+        "30": 10,
+        "31": 10,
+        "32": 10
       }
     },
-    female: {}
+    extroversion: {
+      male: {
+        "0": 1,
+        "1": 1,
+        "2": 1,
+        "3": 1,
+        "4": 2,
+        "5": 2,
+        "6": 3,
+        "7": 3,
+        "8": 4,
+        "9": 4,
+        "10": 4,
+        "11": 5,
+        "12": 5,
+        "13": 5,
+        "14": 5,
+        "15": 6,
+        "16": 6,
+        "17": 6,
+        "18": 7,
+        "19": 7,
+        "20": 8,
+        "21": 9,
+        "22": 9,
+        "23": 10,
+      },
+      female: {
+        "0": 1,
+        "1": 1,
+        "2": 1,
+        "3": 2,
+        "4": 2,
+        "5": 2,
+        "6": 3,
+        "7": 3,
+        "8": 4,
+        "9": 4,
+        "10": 4,
+        "11": 5,
+        "12": 5,
+        "13": 5,
+        "14": 5,
+        "15": 6,
+        "16": 6,
+        "17": 7,
+        "18": 7,
+        "19": 8,
+        "20": 8,
+        "21": 9,
+        "22": 10,
+        "23": 10,
+      }
+    },
+    neuroticism: {
+      male: {
+        "0": 1,
+        "1": 1,
+        "2": 2,
+        "3": 2,
+        "4": 3,
+        "5": 3,
+        "6": 4,
+        "7": 4,
+        "8": 4,
+        "9": 5,
+        "10": 5,
+        "11": 5,
+        "12": 6,
+        "13": 6,
+        "14": 6,
+        "15": 7,
+        "16": 7,
+        "17": 7,
+        "18": 7,
+        "19": 8,
+        "20": 8,
+        "21": 9,
+        "22": 9,
+        "23": 9,
+        "24": 10,
+      },
+      female: {
+        "0": 1,
+        "1": 1,
+        "2": 1,
+        "3": 1,
+        "4": 2,
+        "5": 2,
+        "6": 2,
+        "7": 3,
+        "8": 3,
+        "9": 3,
+        "10": 4,
+        "11": 4,
+        "12": 4,
+        "13": 5,
+        "14": 5,
+        "15": 5,
+        "16": 5,
+        "17": 6,
+        "18": 6,
+        "19": 7,
+        "20": 7,
+        "21": 8,
+        "22": 8,
+        "23": 9,
+        "24": 10,
+      }
+    },
+    lie: {
+      male: {
+        "0": 1,
+        "1": 1,
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 4,
+        "6": 5,
+        "7": 5,
+        "8": 5,
+        "9": 6,
+        "10": 6,
+        "11": 7,
+        "12": 7,
+        "13": 8,
+        "14": 8,
+        "15": 9,
+        "16": 9,
+        "17": 10,
+        "18": 10,
+        "19": 10,
+        "20": 10,
+        "21": 10,
+      },
+      female: {
+        "0": 1,
+        "1": 1,
+        "2": 2,
+        "3": 2,
+        "4": 3,
+        "5": 4,
+        "6": 4,
+        "7": 5,
+        "8": 5,
+        "9": 6,
+        "10": 6,
+        "11": 7,
+        "12": 7,
+        "13": 8,
+        "14": 9,
+        "15": 9,
+        "16": 10,
+        "17": 10,
+        "18": 10,
+        "19": 10,
+        "20": 10,
+        "21": 10,
+      }
+    }
+  },
+  pupils: {
+    psyhotyzm: {
+      male: {},
+      female: {}
+    },
+    extroversion: {
+      male: {},
+      female: {}
+    },
+    neuroticism: {
+      male: {},
+      female: {}
+    },
+    lie: {
+      male: {},
+      female: {}
+    }
+  },
+  adults: {
+    psyhotyzm: {
+      male: {},
+      female: {}
+    },
+    extroversion: {
+      male: {},
+      female: {}
+    },
+    neuroticism: {
+      male: {},
+      female: {}
+    },
+    lie: {
+      male: {},
+      female: {}
+    }
   }
 }
 
@@ -209,6 +439,8 @@ const resultBtn = document.getElementById('result');
 
 //results boxes
 const resultBox = document.querySelector('.result-box');
+const titleGender = resultBox.querySelector('span.gender');
+const titleGroup = resultBox.querySelector('span.group');
 
 const calculateAnswers = array => {
   return [{
@@ -285,38 +517,44 @@ const changeAnswer = e => {
   document.querySelectorAll('.answers button')[index].focus();
 }
 
+const getSten = (group, scale, gender, rawR, stens) => stens[group][scale][gender][rawR];
+
 const displayResult = (arr) => {
   const group = document.querySelector("#people-group").value;
   const gender = document.querySelector('[name="gender"]:checked').value;
-  // const resultDiv= document.querySelector('.result-box');
+  const resultSpans = document.querySelectorAll('.result-box span[data-type]');
 
-  console.log(group, gender);
-
+  console.log(resultSpans);
   const rawPsyhoResult = [...arr.filter(item => item.group === 'psyhotyzm')[0].items, ...arr.filter(item => item.group === 'psyhotyzm')[1].items].length;
-
   const rawExtroResult = [...arr.filter(item => item.group === 'extroversion')[0].items, ...arr.filter(item => item.group === 'extroversion')[1].items].length;
-
   const rawNeuroResult = [...arr.filter(item => item.group === 'neuroticism')[0].items].length;
-
   const rawLieResult = [...arr.filter(item => item.group === 'lie')[0].items, ...arr.filter(item => item.group === 'lie')[1].items].length;
 
-  // const
+  const results = {
+    rawPsyhoResult,
+    stenPsyhoResult: getSten(group, 'psyhotyzm', gender, rawPsyhoResult, stens),
+    rawExtroResult,
+    stenExtroResult: getSten(group, 'extroversion', gender, rawExtroResult, stens),
+    rawNeuroResult,
+    stenNeuroResult: getSten(group, 'neuroticism', gender, rawNeuroResult, stens),
+    rawLieResult,
+    stenLieResult: getSten(group, 'lie', gender, rawLieResult, stens)
+  }
 
+  resultSpans.forEach(span => span.textContent = results[span.dataset.type])
 
-
-  // console.log('psyhotyzm: ' + rawPsyhoResult, '\nextroversion: ' + rawExtroResult, '\nneuroticism: ' + rawNeuroResult, '\nlie: ' + rawLieResult);
+  titleGender.textContent = gender === 'male' ? 'męszczyzn' : 'kobiet';
+  titleGroup.textContent = `${group === 'students'?'Studenci':(group === 'pupils'?'Uczniowie':(group==='adults'?'Dorośli':''))}`
 
   resultBox.classList.add('visible');
   document.body.classList.add('no-scroll')
 }
 
 const resetAnswers = array => {
-  console.log(array);
   array.forEach(el => {
     el.className = "default";
     el.answer = "none";
   });
-  console.log(array);
 
   localStorage.setItem('answers', JSON.stringify(array));
   render(array);
@@ -332,11 +570,6 @@ const closeResultBox = e => {
 answersDiv.addEventListener('click', changeAnswer);
 resultBtn.addEventListener('click', () => displayResult(calculateAnswers(answers)));
 resetBtn.addEventListener('click', () => resetAnswers(answers));
-
-
-// document.querySelector('.close-results').addEventListener('click', ({
-//   target
-// }) => target)
 
 resultBox.addEventListener('click', closeResultBox)
 resultBox.addEventListener('touchstart', closeResultBox)
